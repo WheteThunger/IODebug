8da543267fabbd3203b9a6066bf9bf03ee507209
WhiteThunder <whetethunger@gmail.com>
2023-11-28 20:01:28 -0800

    Fix for Rust update, redesign with new features

--- IODebug.cs
+++ IODebug.cs
@@ -1,122 +1,355 @@
 ﻿using Oxide.Core.Libraries.Covalence;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
+using static IOEntity;
 
 namespace Oxide.Plugins
 {
-    [Info("IO Debug", "WhiteThunder", "0.4.0")]
+    [Info("IO Debug", "WhiteThunder", "0.5.0")]
     [Description("Helps debugging electrical entities not working.")]
     internal class IODebug : CovalencePlugin
     {
-        private const float CheckIntervalSeconds = 10;
-        private const int Threshold = 100;
+        #region Fields
 
-        private void OnServerInitialized()
-        {
-            timer.Every(CheckIntervalSeconds, () =>
-            {
-                var queue = IOEntity._processQueue;
-                if (queue.Count < Threshold)
-                    return;
+        private const int MaxQueueOccupantsToShow = 5;
 
-                var entity = queue.Peek();
-                if (entity == null)
-                    return;
+        private readonly Dictionary<IOEntity, int> _queueCountByEntity = new Dictionary<IOEntity, int>();
 
-                // Ignore unowned entities since they are probably at monuments and not the cause.
-                if (entity.OwnerID == 0)
-                    return;
+        private Dictionary<QueueType, Queue<IOEntity>> _queues = typeof(IOEntity)
+            .GetField("_processQueues", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+            ?.GetValue(null) as Dictionary<QueueType, Queue<IOEntity>>;
 
-                for (var inputSlotIndex = 0; inputSlotIndex < entity.inputs.Length; inputSlotIndex++)
-                {
-                    var inputSlot = entity.inputs[inputSlotIndex];
-                    var sourceEntity = inputSlot.connectedTo.Get();
-                    if (sourceEntity == null)
-                        continue;
+        #endregion
 
-                    foreach (var outputSlot in entity.outputs)
-                    {
-                        var destinationEntity = outputSlot.connectedTo.Get();
-                        if (destinationEntity != sourceEntity)
-                            continue;
-
-                        entity.UpdateFromInput(0, inputSlotIndex);
-
-                        sourceEntity.outputs[inputSlot.connectedToSlot].Clear();
-                        inputSlot.Clear();
-
-                        sourceEntity.SendChangedToRoot(forceUpdate: true);
-                        entity.MarkDirtyForceUpdateOutputs();
-
-                        sourceEntity.SendNetworkUpdate();
-                        entity.SendNetworkUpdate();
-
-                        var pos = entity.transform.position;
-                        LogWarning($"Electrical queue size is {queue.Count}. Disconnected suspicious entity ({entity.name}) at {pos.x:f1},{pos.y:f1},{pos.z:f1}.");
-                        timer.Once(3, () => LogWarning($"Electrical queue size is now {queue.Count}."));
-                        return;
-                    }
-                }
-            });
-        }
+        #region Commands
 
         [Command("iodebug")]
-        private void CommandIODebug(IPlayer player)
+        private void CommandIODebug(IPlayer player, string cmd, string[] args)
+        {
+            if (!VerifyPermission(player))
+                return;
+
+            if (args.Length == 0)
+            {
+                player.Reply(GetOverallReport());
+                return;
+            }
+
+            QueueType queueType;
+            if (!Enum.TryParse(args[0], ignoreCase: true, result: out queueType))
+            {
+                player.Reply($"Syntax: {cmd} <queue name>");
+                return;
+            }
+
+            var queue = GetQueue(queueType);
+            if (queue == null)
+            {
+                player.Reply($"Queue {queueType} not found.");
+                return;
+            }
+
+            player.Reply(GetQueueReport(queueType, queue));
+        }
+
+        [Command("iodebug.unplug")]
+        private void CommandUnplug(IPlayer player, string cmd, string[] args)
+        {
+            if (!VerifyPermission(player))
+                return;
+
+            uint netId;
+            if (args.Length < 1 || !uint.TryParse(args[0], out netId))
+            {
+                player.Reply($"Syntax: {cmd} <entity net ID>");
+                return;
+            }
+
+            var ioEntity = BaseNetworkable.serverEntities.Find(new NetworkableId(netId)) as IOEntity;
+            if (ioEntity == null || ioEntity.IsDestroyed)
+            {
+                player.Reply($"No IOEntity found with net ID {netId}.");
+                return;
+            }
+
+            if (UnplugIOEntity(ioEntity))
+            {
+                player.Reply($"Unplugged inputs to entity {netId} ({ioEntity.ShortPrefabName}).");
+            }
+            else
+            {
+                player.Reply($"Entity {netId} ({ioEntity.ShortPrefabName}) has nothing plugged into its inputs.");
+            }
+        }
+
+        [Command("iodebug.reducequeue")]
+        private void CommandReduceQueue(IPlayer player, string cmd, string[] args)
+        {
+            QueueType queueType;
+            Queue<IOEntity> queue;
+            if (!VerifyPermission(player)
+                || !VerifyQueueExists(player, cmd, args, out queueType, out queue)
+                || !VerifyQueueNotEmpty(player, queueType, queue))
+                return;
+
+            var originalSize = queue.Count;
+            RemoveConsecutiveDuplicates(queue);
+
+            var newSize = queue.Count;
+            if (originalSize == newSize)
+            {
+                player.Reply($"We were unable to reduce the {queueType} queue to due to no consecutive duplicates.");
+                return;
+            }
+
+            player.Reply($"{queueType} queue reduced from {originalSize} to {newSize}");
+        }
+
+        [Command("iodebug.clearqueue")]
+        private void CommandClearQueue(IPlayer player, string cmd, string[] args)
         {
             if (!player.IsServer && !player.IsAdmin)
                 return;
 
-            var queue = IOEntity._processQueue;
-            if (queue.Count == 0)
-            {
-                player.Reply($"The IO queue is empty. That means electricity should be working fine.");
+            QueueType queueType;
+            Queue<IOEntity> queue;
+            if (!VerifyQueueExists(player, cmd, args, out queueType, out queue)
+                || !VerifyQueueNotEmpty(player, queueType, queue))
                 return;
+
+            queue.Clear();
+            player.Reply($"Queue {queueType} has been cleared. Entities that were in the queue might be in an incorrect state.");
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private bool VerifyPermission(IPlayer player)
+        {
+            return player.IsServer || player.IsAdmin;
+        }
+
+        private bool VerifyQueueExists(IPlayer player, string cmd, string[] args, out QueueType queueType, out Queue<IOEntity> queue)
+        {
+            queue = null;
+            queueType = QueueType.Generic;
+
+            if (args.Length < 1 || !Enum.TryParse(args[0], ignoreCase: true, result: out queueType))
+            {
+                player.Reply($"Syntax: {cmd} <queue name>");
+                return false;
             }
 
-            var report = $"IO Queue Size: {queue.Count}";
-            report += $"\nframebudgetms: {IOEntity.framebudgetms}";
+            queue = GetQueue(queueType);
+            if (queue == null)
+            {
+                player.Reply($"Queue {queueType} not found.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool VerifyQueueNotEmpty(IPlayer player, QueueType queueType, Queue<IOEntity> queue)
+        {
+            if (queue.Count == 0)
+            {
+                player.Reply($"Queue {queueType} is currently empty. Nothing to do.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private Queue<IOEntity> GetQueue(QueueType queueType)
+        {
+            Queue<IOEntity> queue;
+            return _queues.TryGetValue(queueType, out queue)
+                ? queue
+                : null;
+        }
+
+        private KeyValuePair<IOEntity, int>[] RankQueueMembers(Queue<IOEntity> queue)
+        {
+            _queueCountByEntity.Clear();
+
+            foreach (var entity in queue)
+            {
+                // Ignore unowned entities since they are probably at monuments and not the cause.
+                if (entity.OwnerID == 0)
+                    continue;
+
+                int count;
+                if (!_queueCountByEntity.TryGetValue(entity, out count))
+                {
+                    count = 0;
+                }
+
+                _queueCountByEntity[entity] = ++count;
+            }
+
+            if (_queueCountByEntity.Count == 0)
+                return Array.Empty<KeyValuePair<IOEntity, int>>();
+
+            var result = _queueCountByEntity
+                .OrderByDescending(entry => entry.Value)
+                .ToArray();
+
+            _queueCountByEntity.Clear();
+
+            return result;
+        }
+
+        private string FormatPosition(Vector3 position)
+        {
+            return $"{position.x:f1},{position.y:f1},{position.z:f1}";
+        }
+
+        private string GetIOEntityInfo(IOEntity ioEntity)
+        {
+            var output = "";
+
+            output += $"\nEntity type: {ioEntity.GetType()}";
+            output += $"\nEntity prefab: {ioEntity.PrefabName}";
+            output += $"\nEntity net id: {ioEntity.net.ID}";
+            output += $"\nEntity position: teleportpos {FormatPosition(ioEntity.transform.position)}";
+
+            output += $"\nEntity inputs: {ioEntity.inputs.Count(x => x.connectedTo.Get() != null)}";
+            foreach (var slot in ioEntity.inputs)
+            {
+                var ent = slot.connectedTo.Get();
+                if (ent == null)
+                    continue;
+
+                output += $"\n  - {ent.GetType()} :: {ent.net.ID}";
+            }
+
+            output += $"\nEntity outputs: {ioEntity.outputs.Count(x => x.connectedTo.Get() != null)}";
+            foreach (var slot in ioEntity.outputs)
+            {
+                var ent = slot.connectedTo.Get();
+                if (ent == null) continue;
+                output += $"\n  - {ent.GetType()} :: {ent.net.ID}";
+            }
+
+            output += $"\nEntity valid: {BaseNetworkableEx.IsValid(ioEntity)}";
+            output += $"\nEntity.lastUpdateTime: {ioEntity.lastUpdateTime}";
+            output += $"\nIOEntity.responsetime: {IOEntity.responsetime}";
+            output += $"\nEntity.lastUpdateBlockedFrame: {ioEntity.lastUpdateBlockedFrame}";
+            output += $"\nEntity.HasBlockedUpdatedOutputsThisFrame: {Time.frameCount == ioEntity.lastUpdateBlockedFrame}";
+
+            return output;
+        }
+
+        private string GetOverallReport()
+        {
+            var output = "Showing the size of each IO queue.";
+
+            foreach (var entry in _queues)
+            {
+                output += $"\n- {entry.Key}: {entry.Value.Count}";
+            }
+
+            output += "\nUse iodebug <queue name> (e.g., iodebug ElectricLowPriority) for more info about a specific queue.";
+
+            return output;
+        }
+
+        private string GetQueueReport(QueueType queueType, Queue<IOEntity> queue)
+        {
+            if (queue.Count == 0)
+                return $"The {queueType} queue is empty. That means entities that use that queue should be working fine.";
+
+            var output = $"{queueType} queue size: {queue.Count}";
 
             if (queue.Count > 0)
             {
-                var nextEntity = queue.Peek();
-                if (nextEntity == null)
-                    return;
+                output += $"\n-----Top {queueType} queue occupants-----";
 
-                var pos = nextEntity.transform.position;
-                var positionString = $"{pos.x:f1},{pos.y:f1},{pos.z:f1}";
+                var topRanked = RankQueueMembers(queue)
+                    .Take(MaxQueueOccupantsToShow)
+                    .ToArray();
 
-                report += $"\nEntity type: {nextEntity.GetType()}";
-                report += $"\nEntity prefab: {nextEntity.PrefabName}";
-                report += $"\nEntity net id: {nextEntity.net.ID}";
-                report += $"\nEntity position: teleportpos {positionString}";
-
-                report += $"\nEntity inputs: {nextEntity.inputs.Count(x => x.connectedTo.Get() != null)}";
-                foreach (var slot in nextEntity.inputs)
+                if (topRanked.Length > 0)
                 {
-                    var ent = slot.connectedTo.Get();
-                    if (ent == null)
-                        continue;
+                    foreach (var entry in topRanked)
+                    {
+                        output += $"\n[{entry.Value} times] {entry.Key.ShortPrefabName} @ {FormatPosition(entry.Key.transform.position)}";
+                    }
 
-                    report += $"\n  - {ent.GetType()} :: {ent.net.ID}";
+                    var topEntity = topRanked.FirstOrDefault().Key;
+
+                    output += $"\n\n-----Showing top entity-----";
+                    output += GetIOEntityInfo(topEntity);
+                    output += $"\nTime.frameCount: {Time.frameCount}";
                 }
-
-                report += $"\nEntity outputs: {nextEntity.outputs.Count(x => x.connectedTo.Get() != null)}";
-                foreach (var slot in nextEntity.outputs)
-                {
-                    var ent = slot.connectedTo.Get();
-                    if (ent == null) continue;
-                    report += $"\n  - {ent.GetType()} :: {ent.net.ID}";
-                }
-
-                report += $"\nEntity valid: {BaseNetworkableEx.IsValid(nextEntity)}";
-                report += $"\nEntity.lastUpdateTime: {nextEntity.lastUpdateTime}";
-                report += $"\nIOEntity.responsetime: {IOEntity.responsetime}";
-                report += $"\nEntity.lastUpdateBlockedFrame: {nextEntity.lastUpdateBlockedFrame}";
-                report += $"\nEntity.HasBlockedUpdatedOutputsThisFrame: {Time.frameCount == nextEntity.lastUpdateBlockedFrame}";
-                report += $"\nTime.frameCount: {Time.frameCount}";
             }
 
-            player.Reply(report);
+            return output;
         }
+
+        private bool UnplugIOEntity(IOEntity ioEntity)
+        {
+            var didUnplug = false;
+
+            for (var inputSlotIndex = 0; inputSlotIndex < ioEntity.inputs.Length; inputSlotIndex++)
+            {
+                var inputSlot = ioEntity.inputs[inputSlotIndex];
+                var sourceEntity = inputSlot.connectedTo.Get();
+                if (sourceEntity == null)
+                    continue;
+
+                foreach (var outputSlot in sourceEntity.outputs)
+                {
+                    var destinationEntity = outputSlot.connectedTo.Get();
+                    if (destinationEntity != ioEntity)
+                        continue;
+
+                    ioEntity.UpdateFromInput(0, inputSlotIndex);
+
+                    sourceEntity.outputs[inputSlot.connectedToSlot].Clear();
+                    inputSlot.Clear();
+
+                    sourceEntity.SendChangedToRoot(forceUpdate: true);
+                    ioEntity.MarkDirtyForceUpdateOutputs();
+
+                    sourceEntity.SendNetworkUpdate();
+                    ioEntity.SendNetworkUpdate();
+
+                    didUnplug = true;
+                }
+            }
+
+            return didUnplug;
+        }
+
+        private void RemoveConsecutiveDuplicates(Queue<IOEntity> queue)
+        {
+            var newQueue = queue.ToList();
+            IOEntity previousItem = null;
+            for (var i = newQueue.Count - 1; i >= 0; i--)
+            {
+                var item = newQueue[i];
+                if (item == previousItem)
+                {
+                    newQueue.RemoveAt(i);
+                    continue;
+                }
+
+                previousItem = item;
+            }
+
+            queue.Clear();
+            foreach (var item in newQueue)
+            {
+                queue.Enqueue(item);
+            }
+        }
+
+        #endregion
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,64 @@
 ## Features
 
-- Helps debug electrical entities not working
-- Automatically disconnects some bad circuits
+This plugin helps Rust server owners debug and mitigate issues where electrical entities are not working.
+
+## How to use
+
+If you are seeing IO entities not working (e.g., lights not responding when switches are flipped), install this plugin and run the `iodebug` command. This will print out the size of each IO queue. Since you are here, at least one of these queues will likely be pretty backed up, with hundreds or thousands of items. For each queue that appears backed up, run `iodebug <queue name>` (e.g., `iodebug ElectricLowPriority`) to show the top entities in the queue, as well as detailed information about the top entity. Most likely, the reason the queue is backed up is that one of the entities at the top of the queue (most likely the top one) is somehow getting spam added to the queue faster than the queue can be processed, causing the queue to back up over time.
+
+Once you have identified a suspicious entity, go to the location of that entity with `teleportpos <position>` (e.g., `teleportpos -251.7,76.0,-482.5`), and unplug it with a wire/hose/pipe tool. Alternatively, you can unplug it remotely with `iodebug.unplug <entity net ID>` (e.g., `iodebug.unplug 2537861`). After unplugging it, re-run the `iodebug` command periodically over the next few minutes to confirm that the queue size is going down. It may take minutes or even hours to fully drain a backlogged queue, so the important thing is whether the queue size is going down instead of up. A healthy queue has a size of 0.
+
+Once the queue has begun draining, if it's taking too long to drain, you can try temporarily increasing the following convars to grant the server more performance budget to process the queue faster. Note: Increasing the budget may decrease server FPS, so use with caution, and change it back once the queue reaches 0.
+
+- `ioentity.framebudgetelectrichighpriorityms` (for `ElectricHighPriority` queue)
+- `ioentity.framebudgetelectriclowpriorityms` (for `ElectricLowPriority` queue)
+- `ioentity.framebudgetfluidms` (for `Fluidic` queue)
+- `ioentity.framebudgetkineticms` (for `Kinetic` queue)
+- `ioentity.framebudgetgenericms` (for `Generic` queue)
+- `ioentity.framebudgetindustrialms` (for `Industrial` queue)
+
+The convar value represents how many milliseconds (ms) per frame can be spent processing the queue. For example, if you are aiming for 30 FPS, you want each frame to be processed within approximately 33ms (1000ms / 30 = 33.33ms), so you would want the IO budget to be much lower than that to allow the server to process everything else not IO related. The default budget size is less than or equal to 1ms for every queue. To start with, try increasing the budget by one ms at a time (e.g., `ioentity.framebudgetelectriclowpriorityms 1.5` then `ioentity.framebudgetelectriclowpriorityms 2.5`).
 
 ## Commands
 
-- `iodebug` -- Prints information about the electrical queue. If the queue is empty, almost no information will be printed. If the queue is non-empty, lots of information will be printed.
+- `iodebug` -- Prints the name and size of each IO queue. If a given queue is empty (size 0), that means electricity/fluid/etc. is probably working fine, for entities that use that queue. If the queue is non-empty, lots of information will be printed.
+- `iodebug <queue name>` -- Prints information about a specific queue.
+  - `ElectricHighPriority` -- Used by monument electrical entities (i.e., puzzles).
+  - `ElectricLowPriority` -- Used by other electrical entities (i.e., player deployables).
+  - `Fluidic` -- Used by fluid entities (e.g, water barrels, sprinklers).
+  - `Kinetic` -- Used by kinetic entities (e.g., wheel switch, blast door).
+  - `Generic` -- Not used.
+  - `Industrial` -- Used by industrial entities (e.g, industrial conveyor, industrial crafter).
+- `iodebug.unplug <entity net ID>` -- Disconnects any wires/hoses/pipes that are connected to the entities inputs. This is useful if you want to remotely disconnect an entity without visiting it in-game. The entity net ID will be visible in the output of `iodebug <queue name>`.
+- `iodebug.reducequeue <queue name>` -- Removes consecutive duplicates from the specified IO queue. For example, if the queue consists of `switch1, timer1, timer1, timer1, switch1, switch2`, this command will reduce the queue to `switch1, timer1, switch1, switch2`. This command is believed to be safe, having no adverse effects on the entities in the queue. This may have no effect if the duplicates in the queue are not consecutive.
+- `iodebug.clearqueue <queue name>` -- Clears all entities from the specified IO queue. Caution is advised because this could leave entities that were in the queue in an incorrect state (e.g., not powered when they should have been). This should be used as a last resort if other queue reduction options have been exhausted.
 
-Example output:
+## Example usage
 
 ```
-IO Queue Size: 9
-framebudgetms: 1
+> iodebug
+
+Showing the size of each IO queue.
+- ElectricHighPriority: 0
+- ElectricLowPriority: 1596
+- Fluidic: 0
+- Kinetic: 0
+- Generic: 0
+- Industrial: 0
+Use iodebug <queue name> (e.g., iodebug ElectricLowPriority) for more info about a specific queue.
+```
+
+```
+> iodebug ElectricLowPriority
+
+-----Top ElectricLowPriority queue occupants-----
+[381 times] electrical.blocker.deployed.prefab @ -251.7,76.0,-482.5
+[370 times] sam_site_turret_deployed.prefab @ 195.2,17.7,-143.0
+[263 times] electrical.blocker.deployed.prefab @ 675.0,29.4,975.8
+[132 times] large.rechargable.battery.deployed.prefab @ 777.2,29.3,881.8
+[119 times] autoturret_deployed.prefab @ 157.1,26.3,1167.1
+
+-----Showing top entity-----
 Entity type: ElectricalBlocker
 Entity prefab: assets/prefabs/deployable/playerioents/gates/blocker/electrical.blocker.deployed.prefab
 Entity net id: 2537861
@@ -28,23 +75,3 @@ Entity.lastUpdateBlockedFrame: 4469921
 Entity.HasBlockedUpdatedOutputsThisFrame: False
 Time.frameCount: 4469922
 ```
-
-## How to use
-
-If you are seeing electrical entities not working (e.g., not responding when switches are flipped), install this plugin and run the `iodebug` command. This will print out information about the number of the entities in the processing queue, with additional information about the entity that is at the head of the queue.
-
-A healthy queue should only have a few entities in it. If you see hundreds or thousands of entities in the queue, that means something is probably wrong, and the queue size will likely continue to increase. The root cause of such an increase is usually particular circuits that the game doesn't process efficiently. To resolve, you will need to locate the entities which are the problem and unplug or remove them. The output of the `iodebug` command can help you determine which entity is the problem. If a particular entity is the issue, typically repeat runs of the `iodebug` commands will refer to the same entity.
-
-In some cases, the plugin will auto detect the issue and remedy it for you by disconnecting a suspicious entity. This will show up in the logs like the following.
-
-```
-[IO Debug] Electrical queue size is 120. Disconnected suspicious entity (assets/prefabs/deployable/playerioents/gates/blocker/electrical.blocker.deployed.prefab) at -253.9,74.8,-477.6.
-```
-
-A short time later, an additional message will show up stating the new queue size. If the new queue size is significantly lower than the original queue size, that indicates that automatically disconnecting the suspicious entity has solved the problem, and the queue size will likely continue to decrease.
-
-```
-[IO Debug] Electrical queue size is now 79.
-```
-
-If the queue size has not significantly decreased in the second message, you will likely have to run the `iodebug` command a few times to identify which entity is the problem, then go disconnect or remove it yourself.
